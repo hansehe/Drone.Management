21ef400508fdcf1c488170aeacbd4b5b2e87f8cd
Hans Erik Heggem <heh@dips.no>
2018-04-03 16:49:34 +0200

    changed return result from create and update drone

--- src/Drone.Management.Tests/Drone.Management.API.Tests/DroneControllerTests.cs
+++ src/Drone.Management.Tests/Drone.Management.API.Tests/DroneControllerTests.cs
@@ -62,8 +62,7 @@ namespace Drone.Management.API.Tests
         {
             var eventController = GetDroneController();
             var actionResult = await eventController.CreateDrone(GetDroneDto());
-            var okObjectResult = actionResult.Should().BeOfType<OkObjectResult>().Subject;
-            var result = okObjectResult.Value.Should().BeAssignableTo<DroneDto>().Subject;
+            actionResult.Should().BeOfType<CreatedAtRouteResult>();
         }
 
         [Fact]
@@ -72,7 +71,7 @@ namespace Drone.Management.API.Tests
             var eventController = GetDroneController();
             var actionResult = await eventController.GetDroneIds();
             var okObjectResult = actionResult.Should().BeOfType<OkObjectResult>().Subject;
-            var result = okObjectResult.Value.Should().BeAssignableTo<IEnumerable<IdentityDto>>().Subject;
+            okObjectResult.Value.Should().BeAssignableTo<IEnumerable<IdentityDto>>();
         }
 
         [Fact]
@@ -81,7 +80,7 @@ namespace Drone.Management.API.Tests
             var eventController = GetDroneController();
             var actionResult = await eventController.GetDrone(Guid.NewGuid().ToString());
             var okObjectResult = actionResult.Should().BeOfType<OkObjectResult>().Subject;
-            var result = okObjectResult.Value.Should().BeAssignableTo<DroneDto>().Subject;
+            okObjectResult.Value.Should().BeAssignableTo<DroneDto>();
         }
         
         [Fact]
@@ -89,8 +88,7 @@ namespace Drone.Management.API.Tests
         {
             var eventController = GetDroneController();
             var actionResult = await eventController.UpdateDrone(GetDroneDto());
-            var okObjectResult = actionResult.Should().BeOfType<OkObjectResult>().Subject;
-            var result = okObjectResult.Value.Should().BeAssignableTo<DroneDto>().Subject;
+            actionResult.Should().BeOfType<AcceptedAtRouteResult>();
         }
 
         [Fact]
@@ -99,7 +97,7 @@ namespace Drone.Management.API.Tests
             var eventController = GetDroneController();
             var actionResult = await eventController.DeleteDrone(Guid.NewGuid().ToString());
             var okObjectResult = actionResult.Should().BeOfType<OkObjectResult>().Subject;
-            var result = okObjectResult.Value.Should().BeAssignableTo<string>().Subject;
+            okObjectResult.Value.Should().BeAssignableTo<string>();
         }
     }
 }
